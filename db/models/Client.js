2e072c4b2299e91ba7806a7d7d62925d821abadf
NJAbuin <nabuin@netuniversecorp.com>
2019-11-21 13:48:53 -0300

    no typo

--- db/models/Client.js
+++ db/models/Client.js
@@ -2,7 +2,7 @@ const db = require("../index");
 const S = require("sequelize");
 const crypto = require("crypto");
 
-class Client extends S.Model { }
+class Client extends S.Model {}
 
 Client.init(
   {
@@ -23,7 +23,7 @@ Client.init(
       allowNull: false
     },
     salt: {
-      type: S.salt
+      type: S.STRING
     },
     website: {
       type: S.STRING
@@ -38,16 +38,16 @@ Client.init(
   { sequelize: db, modelName: "client" }
 );
 
-Client.prototype.hashPassword = function (password) {
+Client.prototype.hashPassword = function(password) {
   return crypto
     .createHmac("sha1", this.salt)
     .update(password)
     .digest("hex");
 };
-Client.prototype.randomSalt = function () {
+Client.prototype.randomSalt = function() {
   return crypto.randomBytes(20).toString("hex");
 };
-Client.prototype.validatePassword = function (password) {
+Client.prototype.validatePassword = function(password) {
   let newPassword = this.hashPassword(password);
   return newPassword === this.password;
 };
