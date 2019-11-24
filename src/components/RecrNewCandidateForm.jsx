81c9a7cef57e9b36398d5391fa978a234f824d13
NJAbuin <nabuin@netuniversecorp.com>
2019-11-24 00:03:55 -0300

    error messages on new candidate

--- src/components/RecrNewCandidateForm.jsx
+++ src/components/RecrNewCandidateForm.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { labelInputCreator } from "../../utils";
 import { H1 } from "../templates/H1";
-import CSVReader from "react-csv-reader";
 import Axios from "axios";
+import RecrCsvForm from "./RecrCsvForm";
+import { InfoParagraph } from "../templates/InfoParagraph";
 
 export default function RecrNewCandidateForm(props) {
   const [DNI, setDNI] = React.useState(0);
   const [fullName, setfullName] = React.useState("");
-  const [age, setAge] = React.useState("");
+  const [age, setAge] = React.useState(0);
   const [jobTitle, setJobTitle] = React.useState("");
   const [address, setAddress] = React.useState("");
-  const [expectedSalary, setExpectedSalary] = React.useState("");
+  const [expectedSalary, setExpectedSalary] = React.useState(0);
   const [warningMessage, setWarningMessage] = React.useState("");
-  const [csvValues, setCsvValues] = React.useState([]);
+  const [submitted, setSubmitted] = React.useState(false);
 
   const uploadPDF = e => {
     e.preventDefault();
@@ -21,50 +22,44 @@ export default function RecrNewCandidateForm(props) {
 
   const handleClick = e => {
     e.preventDefault();
-    if (props.match.url === "/recruiter/candidate") {
-      Axios.post("/api/recruiter/candidatos", {
-        DNI,
-        fullName,
-        age,
-        jobTitle,
-        address,
-        expectedSalary
-      }).then(candidate => console.log(candidate));
-    } else if (props.match.path === "/candidatos/edit/:id") {
-      Axios.put(`/api/recruiter/candidatos/edit/${props.match.params.id}`, {
-        DNI,
-        fullName,
-        age,
-        jobTitle,
-        address,
-        expectedSalary
-      }).then(candidate => console.log(candidate));
-    }
+    validateForm(DNI, fullName, age, jobTitle);
+    doTheThing();
   };
 
+  const doTheThing = () => {
+    if (warningMessage === "" && !DNI.length < 8) {
+      if (props.match.url === "/recruiter/candidate") {
+        () =>
+          Axios.post("/api/recruiter/candidatos", {
+            DNI,
+            fullName,
+            age,
+            jobTitle,
+            address,
+            expectedSalary
+          }).then(candidate => {
+            console.log(candidate);
+          });
+      } else if (
+        props.match.path === "/candidatos/edit/:id" &&
+        DNI.length < 8
+      ) {
+        Axios.put(`/api/recruiter/candidatos/edit/${props.match.params.id}`, {
+          DNI,
+          fullName,
+          age,
+          jobTitle,
+          address,
+          expectedSalary
+        }).then(candidate => console.log(candidate));
+      }
+    }
+  };
   const handleChange = e => {
     console.log(e.target.files[0]);
   };
 
-  const papaparseOptions = {
-    header: true,
-    dynamicTyping: true,
-    skipEmptyLines: true
-    // transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
-  };
-
-  const handleForce = data => {
-    setCsvValues(data);
-  };
-
-  const bulkCreateFromCsv = e => {
-    e.preventDefault();
-    Axios.post("/api/recruiter/candidatos/csvImport", [...csvValues]).then(() =>
-      console.log(res.data)
-    );
-  };
-
-  return (
+  const form = (
     <div>
       <form>
         <H1>Ingrese los datos del candidato a agregar:</H1>
@@ -79,28 +74,47 @@ export default function RecrNewCandidateForm(props) {
           <input
             type="file"
             onChange={e => handleChange(e)}
-            /*onClick={e => uploadPDF(e)}*/
+            onClick={e => uploadPDF(e)}
           ></input>
         </label>
         <br />
         <p style={{ color: "red" }}>{warningMessage}</p>
         <button onClick={e => handleClick(e)}>Submit</button>
       </form>
-      <H1>Importe todos sus candidatos facilmente mediante un .csv</H1>
-      <p>
-        Puede extraer los datos de sus candidatos de una planilla propia al
-        exportarla como csv.
-        <br />
-        Debe contener poseer las columnas
-        DNI,fullName,age,jobTitle,address,expectedSalary
-      </p>
-      <CSVReader
-        cssClass="react-csv-input"
-        label="Elije el archivo a subir:  "
-        onFileLoaded={handleForce}
-        parserOptions={papaparseOptions}
-      />
-      <button onClick={e => bulkCreateFromCsv(e)}>Confirmar archivo</button>
+      <hr />
+      <RecrCsvForm />
     </div>
   );
+
+  const success = (
+    <div>
+      <H1>Exito!</H1>
+      <InfoParagraph>
+        El candidato {fullName} con DNI: {DNI} ha sido creado
+      </InfoParagraph>
+    </div>
+  );
+
+  const validateForm = (
+    DNI,
+    fullName,
+    age,
+    jobTitle,
+    address,
+    expectedSalary
+  ) => {
+    if (DNI.length !== 8) {
+      setWarningMessage("Ingrese un DNI valido sin puntos ni guiones.");
+    } else if (fullName.length <= 4 || !fullName.includes(" ")) {
+      setWarningMessage("Ingrese el nombre completo del candidato.");
+    } else if (!age) {
+      setWarningMessage("Ingrese la edad de su candidato.");
+    } else if (jobTitle.length <= 1) {
+      setWarningMessage("Ingrese un titulo valido para su candidato");
+    } else {
+      setWarningMessage("");
+    }
+  };
+
+  return form;
 }
