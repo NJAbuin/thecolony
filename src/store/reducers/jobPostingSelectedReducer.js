856723859e00066380511cf576584032f3476e27
kazy-kode <camilosuner@gmail.com>
2019-11-26 09:31:26 -0500

    a

--- src/store/reducers/jobPostingSelectedReducer.js
+++ src/store/reducers/jobPostingSelectedReducer.js
@@ -5,7 +5,7 @@ const initialState = {};
 export default (state = initialState, action) => {
     switch (action.type) {
         case JOB_POSTING_SELECT:
-            return { ...action.id }
+            return { ...action.jobPost }
 
         default:
             return state;
