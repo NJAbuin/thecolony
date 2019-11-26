ffcb1cd37a93c00ede73b7674839f42075ce52a5
NJAbuin <nabuin@netuniversecorp.com>
2019-11-26 14:33:48 -0300

    relations well done()

--- db/models/index.js
+++ db/models/index.js
@@ -3,12 +3,21 @@ const JobPosting = require("./JobPosting");
 const Client = require("./Client");
 const Candidate = require("./Candidate");
 const Admin = require("./Admin");
+const Candidate_JobPosting = require("./Candidate_JobPosting");
 
+//Client.hasMany(JobPosting);
+//JobPosting.belongsToMany(Recruiter, { through: "recruiterJobPostings" });
+//JobPosting.belongsToMany(Candidate, { through: "candidateJobPostings" });
+//Candidate.belongsToMany(JobPosting, { through: "candidateJobPostings" });
 JobPosting.belongsTo(Client);
-Client.hasMany(JobPosting)
-JobPosting.belongsToMany(Recruiter, { through: 'recruiterJobPostings' });
-JobPosting.belongsToMany(Candidate, { through: 'candidateJobPostings' })
-Candidate.belongsToMany(JobPosting, { through: 'candidateJobPostings' })
+Recruiter.hasMany(JobPosting);
+JobPosting.hasMany(Candidate);
 Candidate.belongsTo(Recruiter);
 
-module.exports = { Candidate, Recruiter, JobPosting, Client };
+module.exports = {
+  Candidate,
+  Recruiter,
+  JobPosting,
+  Client,
+  Candidate_JobPosting
+};
