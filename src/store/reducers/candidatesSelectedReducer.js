6507dd2a40f065e504ce73df7a4df39bc57d256c
manuelanton <s.manuel.anton@gmail.com>
2019-11-27 14:24:13 -0300

    momos

--- src/store/reducers/candidatesSelectedReducer.js
+++ src/store/reducers/candidatesSelectedReducer.js
@@ -1,4 +1,5 @@
 import { CANDIDATE_ADD, CANDIDATE_REMOVE } from "../constants";
+import Candidate from "../../components/Candidate";
 
 const initialState = [];
 
@@ -6,7 +7,12 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case CANDIDATE_ADD:
       return [...state, action.payload];
-
+    case CANDIDATE_REMOVE:
+      let found = state.find(candidate => {
+        return candidate.id == action.payload.id;
+      });
+      let newState = state.filter(candidate => candidate !== found);
+      return [...newState];
     default:
       return state;
   }
