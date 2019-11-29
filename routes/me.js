0a8501d46bd945387be64aa292e972d393d95acc
manuelanton <s.manuel.anton@gmail.com>
2019-11-29 11:46:24 -0300

    a

--- routes/me.js
+++ routes/me.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
 router.get("/", function(req, res) {
-  const { fullName, email, type, id } = req.user;
-  res.send({ fullName, email, type, id });
+  if (req.user) {
+    const { fullName, email, type, id } = req.user;
+    res.send({ fullName, email, type, id });
+  }
 });
 
 module.exports = router;
