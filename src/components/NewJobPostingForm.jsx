6bc3385ae8e33facbba05bd2dbeb12edfab5d412
NJAbuin <nabuin@netuniversecorp.com>
2019-12-02 11:42:00 -0300

    create jobposting alpha

--- src/components/NewJobPostingForm.jsx
+++ src/components/NewJobPostingForm.jsx
@@ -1,21 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { labelInputCreator } from "../../utils/formLoginRegister";
 import { Label } from "../templates/FormLabel";
+import axios from "axios";
 
 export default function NewJobPostingForm() {
   const [title, setTitle] = useState(null);
   const [description, setDescription] = useState(null);
-  const [selectedDate, setSelectedDate] = React.useState(new Date());
   const [startDate, setStartDate] = useState(new Date());
+  const [openings, setOpenings] = useState(0);
+  const [salary, setSalary] = useState(0);
+  const [workload, setWorkload] = useState(0);
+  const [imgURL, setImgURL] = useState(null);
+  const [benefits, setBenefits] = useState(null);
+  const [warningMessage, setWarningMessage] = useState(null);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post("/api/client/jobposting", {
+        title,
+        description,
+        startDate,
+        openings,
+        salary,
+        workload,
+        imgURL,
+        benefits
+      })
+      .then(data => console.log("Created", data));
+  };
+
+  React.useEffect(() => setWarningMessage(null), [
+    title,
+    description,
+    startDate,
+    openings
+  ]);
 
   return (
     <div>
-      <form>
+      <form onSubmit={e => handleSubmit(e)}>
         {labelInputCreator("Nombre de la busqueda ", setTitle)}
         <Label>
           Descripcion: <br />
           (Max 255 caracteres)
-          <input
+          <textarea
             style={{
               width: "100%",
               height: "100px"
@@ -26,6 +55,26 @@ export default function NewJobPostingForm() {
             }}
           />{" "}
         </Label>
+        {labelInputCreator("Fecha de inicio ", setStartDate)}
+        {labelInputCreator("Cantidad de puestos disponibles ", setOpenings)}
+        {labelInputCreator("Salario ", setSalary)}
+        {labelInputCreator("Horas de trabajo al dia ", setWorkload)}
+        {labelInputCreator("Link al logo de la empresa ", setImgURL)}
+        <Label>
+          Beneficios: <br />
+          <textarea
+            style={{
+              width: "100%",
+              height: "100px"
+            }}
+            type="text"
+            onChange={e => {
+              setBenefits(e.target.value);
+            }}
+          />{" "}
+        </Label>
+        {warningMessage}
+        <button>Crear busqueda</button>
       </form>
     </div>
   );
