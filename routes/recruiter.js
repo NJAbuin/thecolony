1c144a9c66e015ef6dd80098787da0f4f0dbb325
NJAbuin <nabuin@netuniversecorp.com>
2019-12-03 15:52:00 -0300

    busquedas sidebar admin

--- routes/recruiter.js
+++ routes/recruiter.js
@@ -4,10 +4,7 @@ const passport = require("../db/passport/");
 const chalk = require("chalk");
 const dateFormat = require("dateformat");
 
-
-
-router.post("/jobpostings", function (req, res) {
-  console.log(chalk.bgRed(JSON.stringify(req.body)));
+router.post("/jobpostings", function(req, res) {
   JobPosting.findOne({ where: { id: req.body.id } })
     .then(job => {
       req.body.newCandidates.map(c => {
@@ -20,7 +17,7 @@ router.post("/jobpostings", function (req, res) {
 
 //agregar un candidato desde el detalle de singleJobPosting
 
-router.post("/jobpostings/:id", function (req, res) {
+router.post("/jobpostings/:id", function(req, res) {
   JobPosting.findOne({ where: { id: req.params.id } }).then(job => {
     Candidate.findOne({ where: { id: req.body.id } }).then(candidate => {
       job.addCandidate(candidate).then(() => {
@@ -30,9 +27,7 @@ router.post("/jobpostings/:id", function (req, res) {
   });
 });
 
-
-
-router.get("/jobpostings/:jobID/:candidateID/report", function (req, res) {
+router.get("/jobpostings/:jobID/:candidateID/report", function(req, res) {
   Report.findOne({
     where: {
       candidateID: req.params.candidateID,
@@ -41,7 +36,7 @@ router.get("/jobpostings/:jobID/:candidateID/report", function (req, res) {
   }).then(report => res.send(report.informe));
 });
 
-router.post("/jobpostings/:jobID/:candidateID/report", function (req, res) {
+router.post("/jobpostings/:jobID/:candidateID/report", function(req, res) {
   Report.create({
     candidateID: req.params.candidateID,
     jobPostingID: req.params.jobID,
