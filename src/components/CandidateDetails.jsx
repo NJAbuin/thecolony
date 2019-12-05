c024a13beaf8c0f66d813ff5fa160ee615d70390
manuelanton <s.manuel.anton@gmail.com>
2019-12-05 14:22:54 -0300

    comm succc, falta baccccccccccccccc

--- src/components/CandidateDetails.jsx
+++ src/components/CandidateDetails.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import { CandidatePosting } from "../components/CandidatePosting";
+
 import { CandidateStyle } from "../templates/Candidates";
 import { candidateFetchDetails } from "../store/actions/candidates";
 
@@ -14,7 +16,7 @@ export function CandidateDetails({ candidateDetails }) {
     CV,
     address,
     expectedSalary,
-    jobpostingId
+    jobpostings
   } = candidateDetails;
 
   const showCV = () => window.open(`${CV.replace("dist", "")}`, "_blank");
@@ -22,16 +24,20 @@ export function CandidateDetails({ candidateDetails }) {
   return Object.keys(candidateDetails).length == 0 ? (
     "No details found"
   ) : (
-      <CandidateStyle>
-        <h3>{fullName}</h3>
-        <span>{jobTitle}</span>
-        <br />
-        <button onClick={e => showCV(e)}>Ver CV</button>
-        <p>BUSQUEDAS ASIGNADAS</p>
-        <br />
-        {jobpostingId}
-      </CandidateStyle>
-    );
+    <CandidateStyle>
+      <h3>{fullName}</h3>
+      <span>{jobTitle}</span>
+      <br />
+      <button onClick={e => showCV(e)}>Ver CV</button>
+      {jobpostings.length > 0
+        ? jobpostings.map(posting => (
+            <CandidatePosting posting={posting} key={posting.id} />
+          ))
+        : null}
+
+      <br />
+    </CandidateStyle>
+  );
 }
 
 const mapStateToProps = ({ candidateDetails }) => ({
