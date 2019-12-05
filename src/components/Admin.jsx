c9c869cf30ff46699ccfef99184fe05974fbaef8
NJAbuin <nabuin@netuniversecorp.com>
2019-12-05 12:29:15 -0300

    hjafsjafsadgsjadgsjl

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-import { AdminStyle } from '../templates/AdminStyle'
+import { AdminStyle } from "../templates/AdminStyle";
 function Admin(props) {
-
-    return (<AdminStyle>
-        {Object.keys(props.admin).map(prop => <p key={prop}>{prop}</p>)}
+  return (
+    <AdminStyle>
+      {Object.keys(props.admin).map(prop => (
+        <p style={{ color: "white" }} key={prop}>
+          {prop}
+        </p>
+      ))}
     </AdminStyle>
-    );
+  );
 }
 
-export default Admin
+export default Admin;
