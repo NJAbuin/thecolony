694636c13bb9ca7360850be7f491964f60c16a3f
NJAbuin <nabuin@netuniversecorp.com>
2019-12-10 18:46:15 -0300

    cleaner

--- src/templates/RecruiterStyle.js
+++ src/templates/RecruiterStyle.js
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 
 export const RecruiterStyle = styled.div`
-  background-image: linear-gradient(-90deg, rgb(${props => props.theme.CoolGray}), rgb(${props => props.theme.JadeGreen})) ;
+  background-image: linear-gradient(
+    -90deg,
+    rgb(${props => props.theme.CoolGray}),
+    rgb(${props => props.theme.JadeGreen})
+  );
   border: 3px solid rgb(${props => props.theme.RichBlack});
   border-radius: 6px;
   justify-self: center;
-  height: 8rem;
+  height: 11.5rem;
   width: 95%;
   margin: 2.5%;
   display: block;
