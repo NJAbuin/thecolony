39e46661d1d8f76bd8d174281c1ec2b3fa0bcb62
NJAbuin <nabuin@netuniversecorp.com>
2019-12-11 13:41:58 -0300

    client can see jp details

--- src/containers/ClientContainer.jsx
+++ src/containers/ClientContainer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { Route, Switch, Redirect } from "react-router-dom";
 
+import JobPostingDetails from "../components/JobPostingDetails";
+
 //import DashboardClient from "../containers/DashboardClient";
 import ClientJobPostings from "./ClientJobPostings";
 import NewJobPostingForm from "../components/NewJobPostingForm";
@@ -14,6 +16,10 @@ function ClientContainer() {
       <Route
         path="/auth/client/jobpostings/new"
         component={NewJobPostingForm}
+      />
+      <Route
+        path="/auth/client/jobpostings/:id"
+        component={JobPostingDetails}
       />
       <Route path="/auth/client/jobpostings" component={ClientJobPostings} />
       <Route path="/auth/client/candidates/:id" component={CandidateDetails} />
