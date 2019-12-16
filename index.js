3c8ac135169b5644b7fd1e0c251f0f135ab2f49f
NJAbuin <53914955+NJAbuin@users.noreply.github.com>
2019-12-16 17:36:47 -0300

    cleaner

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const db = require("../db");
+const chalk = require("chalk");
+const passport = require("../db/passport");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const routes = require("../routes");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//bodyṕarser config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//logger
+app.use(morgan("tiny"));
+app.use(express.static("dist"));
+
+//passport
+app.use(
+  session({ secret: "cualquierCosa", resave: false, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+
+//modular routes
+/* app.use("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../dist/index.html"))
+); */
+app.use("/api", routes);
+
+app.use("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../dist/index.html"))
+);
+
+//sync database then start server
+db.sync({ force: false })
+  .then(() => {
+    console.log(chalk.bgGreen.black("Connected to database..."));
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+  })
+  .catch(console.error); //error catcher
